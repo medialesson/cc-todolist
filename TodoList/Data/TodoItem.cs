6d19da96dfc56bf10f482356da1065325bdc23cb
Gino Messmer <git@ginomessmer.me>
2019-02-24 18:24:13 +0100

    Add todo item and repository

---
+++ TodoList/Data/TodoItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.Data
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime AddedAt { get; set; }
+
+        public TodoItem()
+        {
+            Id = new Guid().ToString();
+            AddedAt = DateTime.Now;
+        }
+    }
+}
---
+++ TodoList/Repositories/TodoRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TodoList.Data;
+
+namespace TodoList.Repositories
+{
+    public class TodoRepository
+    {
+        private IList<TodoItem> _todoList;
+
+        public TodoRepository()
+        {
+            _todoList = new List<TodoItem>();
+        }
+
+        public void AddItem(TodoItem item)
+        {
+            _todoList.Add(item);
+        }
+
+        public void AddItem(string title, string content = "", bool isCompleted = false)
+        {
+            AddItem(new TodoItem()
+            {
+                Title = title,
+                Content = content,
+                IsCompleted = isCompleted
+            });
+        }
+
+        public void RemoveItem(TodoItem item)
+        {
+            _todoList.Remove(item);
+        }
+
+        public void RemoveItemById(string id)
+        {
+            var item = _todoList.FirstOrDefault(i => i.Id == id);
+            if (item == null)
+                return;
+
+            _todoList.Remove(item);
+        }
+
+        public IEnumerable<TodoItem> GetAllItems()
+        {
+            return _todoList.ToList();
+        }
+    }
+}
