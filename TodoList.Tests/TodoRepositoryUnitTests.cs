41e0ead5caf5eb5198c7404afd81fb353d36cbf9
Gino Messmer <git@ginomessmer.me>
2019-03-03 17:36:17 +0100

    Add unit tests for todo repository and commands

---
+++ TodoList.Tests/TodoRepositoryUnitTests.cs
@@ -0,0 +1,38 @@
+using System;
+using TodoList.Data;
+using TodoList.Repositories;
+using Xunit;
+
+namespace TodoList.Tests
+{
+    public class TodoRepositoryUnitTests
+    {
+        [Fact]
+        public void ShouldAddNewItemAndValidate()
+        {
+            // Init repository
+            ITodoRepository repository = new TodoRepository();
+            
+            // Init expected variables
+            string expectedTitle = "Finish unit tests";
+            string expectedContent = "Generic description goes here";
+
+            // Set up new item
+            var item = new TodoItem()
+            {
+                Title = expectedTitle,
+                Content = expectedContent
+            };
+
+            // Add new item to repository, fetch it by ID
+            var newItemId = repository.AddItem(item).Id;
+            var foundItem = repository.GetItem(newItemId);
+
+            // Make assertions and check whether new item
+            // corresponds to the found item
+            Assert.Equal(newItemId, foundItem.Id);
+            Assert.Equal(expectedTitle, foundItem.Title);
+            Assert.Equal(expectedContent, foundItem.Content);
+        }
+    }
+}
